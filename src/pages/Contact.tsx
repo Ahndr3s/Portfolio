5065a80af35adf85eefa82e52e6a233edaadb725
Andres <ahndrsx@gmail.com>
2026-07-01 18:16:15 -0600

    general mocking done

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,12 +1,44 @@
 import "./Contact.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export const Contact = () => {
+   const gitHubUrl = "https://github.com/Ahndr3s/Portfolio";
+   const linkedInUrl = "https://www.linkedin.com/in/andres-jacinto/";
+ 
   return (
-    <div className="contact-container">
-      <h1 className="title-container">
-        Work With Me<span className="cursor">_</span>
-      </h1>
-      <div className="button-row"></div>
+    <div className="contact-container ">
+      <div className="cont-title-container">
+        <div className="cont-title-wrap">
+          <h1>Work</h1>
+          <h1>With</h1>
+        </div>
+        <div className="cmd-title">
+          <h1 className="cont-title">Me</h1>
+        </div>
+      </div>
+      <div className="btns-container shadow-lg hover:shadow-xl">        
+        <div className="button-row">
+        <a href={linkedInUrl} target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon
+                      icon={faLinkedin}
+                      size="2x"
+                      color="#000"
+                      style={{ marginLeft: "1rem" }}
+                    />
+                  </a>
+        </div>
+        <div className="button-row">
+        <a href={gitHubUrl} target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon
+                      icon={faGithub}
+                      size="2x"
+                      color="#000"
+                      style={{ marginLeft: "1rem" }}
+                    />
+                  </a>
+        </div>
+      </div>
       <div className="email-banner"></div>
     </div>
   );
